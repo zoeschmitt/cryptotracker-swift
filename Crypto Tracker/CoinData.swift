2505a72764d5b7493205ef0db0f2f2ab34e4c428
Zoe Schmitt <mica@Zoes-MacBook-Pro.local>
2019-03-18 21:54:25 -0500

    api setup

--- Crypto Tracker/CoinData.swift
+++ Crypto Tracker/CoinData.swift
@@ -7,18 +7,62 @@
 //
 
 import UIKit
+import Alamofire
+//all we need to do to add a new crypto is to add it to the symbols array and add the image with the same symbol name in assets.
 
 class CoinData {
-    static let shared = CoinData()
-    var coins = [Coin]()
     
+    static let shared = CoinData()      //singleton
+    var coins = [Coin]()        //array of Coins
+    weak var delegate : CoinDataDelegate?
+    
+    //private init is an initializer for everytime CoinData is called
     private init() {
-        let symbols = ["BTC", "ETH", "LTC"]
+        let symbols = ["BTC", "ETH", "LTC"]     //symbols for cryptos
         
-        for                                 //what we are doing here is saying that when we start the singleton for
-                                            //CoinData, we wanna make 3 coin objects with these symbols and pass them
-                                            //to the coins array
+        //for loop symbol that iterates through our symbols array
+        for symbol in symbols {
+            
+            let coin = Coin(symbol: symbol) //let coin = the first symbol (BTC) this passes it through to Coin class
+            coins.append(coin)      //append new coin to coins array
+        }
     }
+    
+    
+    //this function gathers all cryptos and requests them from the api
+    func getPrices() {
+        
+        var listOfSymbols = ""
+        
+        //iterates in coins array, if its not the last one it will place a comma in between
+        //this makes the listOfSymbols witch will be put in the alamo request
+        for coin in coins {
+            listOfSymbols += coin.symbol
+            
+            if coin.symbol != coins.last?.symbol {
+                listOfSymbols += ","
+            }
+        }
+        //requesting for crypto information and dealing with the response in order to get the prices and put them in price var
+        Alamofire.request("https://min-api.cryptocompare.com/data/pricemulti?fsyms=\(listOfSymbols)&tsyms=USD").responseJSON { (response)
+            in
+            if let json = response.result.value as? [String: Any] {
+                for coin in self.coins {
+                    if let coinJSON = json[coin.symbol] as? [String: Double] {
+                        if let price = coinJSON["USD"] {
+                            coin.price = price
+                        }
+                    }
+                }
+                self.delegate?.newPrices?() //calling the newPrices func to reload the data
+            }
+        }
+    }
+}
+
+@objc protocol CoinDataDelegate: class {
+    @objc optional func newPrices()
+    
 }
 
 class Coin {
@@ -27,4 +71,16 @@ class Coin {
     var price = 0.0
     var amount = 0.0
     var historicalData = [Double]()
+    
+    //initializer function for a Coin
+    init(symbol: String) {
+        self.symbol = symbol    //says the symbol in Coin is now equal to symbol passed through
+        
+        //if statement that says if there is an image equal to the symbol, then make the image in Coin equal to that
+        if let image = UIImage(named: symbol) {
+            self.image = image      //setting image in Coin equal to image
+        }
+    }
 }
+
+
--- Crypto Tracker/CryptoTableViewController.swift
+++ Crypto Tracker/CryptoTableViewController.swift
@@ -8,83 +8,39 @@
 
 import UIKit
 
-class CryptoTableViewController: UITableViewController {
+class CryptoTableViewController: UITableViewController, CoinDataDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        CoinData.shared.delegate = self //saying the delegate var in CoinData is equal to cryptoTableViewController class
+        CoinData.shared.getPrices()     //go ahead and load the prices
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    //this gets called when we get new data from the api in coinData
+    func newPrices() {
+        tableView.reloadData()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        //calls CoinData singleton and counts the amount of symbols there are in the coins array
+        //to decide how many rows to set.
+        return CoinData.shared.coins.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+       
+        let cell = UITableViewCell()    //cell = to an empty UITableViewCell
+        
+        //let coin = instance of CoinData singleton coins array row wise
+        let coin = CoinData.shared.coins[indexPath.row]
+        cell.textLabel?.text = "\(coin.symbol) - \(coin.price)"     //text label = coin symbol
+        cell.imageView?.image = coin.image      //image view = coin image
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
